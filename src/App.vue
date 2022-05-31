161a214bebac689f3cf480431762d616622248a1
Jhony Caro <jhonycaro1998@gmail.com>
2022-05-31 10:49:55 -0500

    tarea 1

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+    <h1 class="texto-rojo">{{ nombre }}</h1>
+    <h2 :class="clasePruebas" v-text="codigo"></h2>
+    <p v-html="textoHtml"></p>
+    <input class="input-grande" type="text" v-model="img" />
+    <br />
+    <img :src="img" alt="" />
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      nombre: "Jhony Alejandro Caro Umbariba",
+      codigo: "20191020055",
+      textoHtml: "<h1>Texto probando el v-html.</h1>",
+      img: "https://picsum.photos/500/200",
+      clasePruebas: "texto-azul",
+    };
+  },
+};
+</script>
+<style>
+.texto-azul {
+  color: blue;
+}
+.texto-rojo {
+  color: red;
+  background-color: black;
+}
+.input-grande {
+  width: 20%;
+  height: 25px;
+  margin: 5px;
+}
+</style>
